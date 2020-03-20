35b8d64eb86481ab6bba368562970cd149f2347c
dasome29 <dasome29@hotmail.com>
2020-03-20 10:16:25 -0600

    Added methods to VSPtr.h related to operator overload.

--- VSPtr.h
+++ VSPtr.h
@@ -5,8 +5,51 @@
 #ifndef TEST_VS_VSPTR_H
 #define TEST_VS_VSPTR_H
 
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T>
 
 class VSPtr {
+private:
+
+    T *ptr;
+
+public:
+    explicit VSPtr(T *p = NULL) {
+        ptr = p;
+    }
+
+    VSPtr(const VSPtr &p) {
+        ptr = p;
+    }
+
+    ~VSPtr() {
+        delete (ptr);
+    }
+
+    T &operator*() {
+
+    }
+
+    T &operator&() {
+
+    }
+
+    VSPtr &operator=(T *element) noexcept {
+
+    }
+
+    void *operator new(size_t size) {
+
+    }
+
+    void operator delete(void *p) {
+
+    }
+
 
 };
 
